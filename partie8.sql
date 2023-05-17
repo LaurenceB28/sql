10b72efd19499bee8f2473bb8825af3113a89e18
LaurenceB28 <laurence.brd@gmail.com>
2023-05-17 10:03:12 +0200

    sql exo8

--- partie8.sql
+++ partie8.sql
@@ -1,13 +1,26 @@
 /*Afficher tous les frameworks associés à leurs langages. Si un langage n' a pas de framework l'afficher aussi.*/
 
-SELECT `languages`.`name` AS `language`, `frameworks`. `name` AS `frameworks`
+SELECT `languages`.`name` AS `language`, `frameworks`. `name` AS `framework`
 FROM `languages`
-LEFT JOIN `frameworks` ON `languages`.`id` = `frameworks`.`languagesid`;
+LEFT JOIN `frameworks` ON `languages`.`id` = `frameworks`.`languagesId`;
 
 /*Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework ne pas l'afficher.*/
-SELECT `languages`.`name` AS `language`, `frameworks`. `name` AS `frameworks`
+
+SELECT `languages`.`name` AS `language`, `frameworks`. `name` AS `framework`
 FROM `languages`
-INNER JOIN `frameworks` ON `languages`.`id` = `frameworks`.`languagesid`;
+INNER JOIN `frameworks` ON `languages`.`id` = `frameworks`.`languagesId`;
+
 /*Afficher le nombre de framework qu'a un langage.*/
+SELECT `languages`.`name` AS `language`, COUNT(`frameworks`.`languagesId`) AS`frameworksNb`
+FROM `languages`
+LEFT JOIN `frameworks` ON `languages`.`id` = `frameworks`.`languagesId`;
+GROUP BY `languages`.`name` 
+ORDER BY `frameworksNb` DESC;
 
 /*Afficher les langages ayant plus de 3 frameworks.*/
+SELECT `languages`.`name` AS `language`, COUNT(`frameworks`.`languagesId`) AS`frameworksNb`
+FROM `languages`
+LEFT JOIN `frameworks` ON `languages`.`id` = `frameworks`.`languagesId`;
+GROUP BY `language`
+HAVING `frameworksNb`>3
+ORDER BY `frameworksNb` DESC;
